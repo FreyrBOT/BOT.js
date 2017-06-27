e2670eb60b2accbd15c74fc32465f65384d5d8a6
FreyrBOT <freyr.bot@seznam.cz>
2017-06-27 20:31:49 +0200

    Update .gitignore.js

--- .gitignore/.gitignore.js
+++ .gitignore/.gitignore.js
@@ -24,21 +24,19 @@
           window.bot.chatUtilities.spam.push(spamWords[i]);
         }
 
-        // Example code for a bot command:
-        afk.commands.afkCommand = {
-            command: 'afk',  // The command to be called. With the standard command literal this would be: !afk
-            rank: 'user', // Minimum user permission to use the command
-            type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-              functionality: function (chat, cmd) {
-                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                if (!bot.commands.executable(this.rank, chat)) return void (0);
-                else {
-                var currentDJ = API.getDJ().username;
-                var user = chat.un; 
-                  API.sendChat("/em @" + user + "see you");
-                }
-              }
-            };
+              // Example code for a bot command:
+        bot.commands.afkCommand = {
+          command: 'afk',  // The command to be called. With the standard command literal this would be: !afk
+          rank: 'user', // Minimum user permission to use the command
+          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+          functionality: function (chat, cmd) {
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
+            else {
+              API.sendChat("/me See you!!!");
+            }
+          }
+        };
 
         // Load the chat package again to account for any changes
         bot.loadChat();
