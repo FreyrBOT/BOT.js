8d04dbafce30be861de3c671df799c01bf3cddd0
FreyrBOT <freyr.bot@seznam.cz>
2017-07-12 17:49:45 +0200

    Update .gitignore.js

--- .gitignore/.gitignore.js
+++ .gitignore/.gitignore.js
@@ -99,7 +99,7 @@
       themeLink: null,
       fbLink: "https://www.facebook.com/BezzubkaLair/?notif_t=page_name_change&notif_id=1491769997319267",
       youtubeLink: null,
-      website: "https://discordapp.com/channels/327229163847155713/327229378436005890",
+      website: "http://i.imgur.com/YP5ifIO.jpg",
       intervalMessages: ["Thank you for your support! With love #BHN <3"],
       messageInterval: 40,
       songstats: false,
